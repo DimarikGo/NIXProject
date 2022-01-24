c5445e6ebc7d35b2422a8252f4a1457832fa0852
DimarikGo <95853379+DimarikGo@users.noreply.github.com>
2022-01-24 17:29:14 +0200

    Update post_mysql.go

--- pkg/repository/post_mysql.go
+++ pkg/repository/post_mysql.go
@@ -41,31 +41,4 @@ func (p *PostMysql) AddP(post Rest_Api.Post) (int, error) {
 	return post.Id, nil
 }
 
-//defer rows.Close()
-//if err != nil {
-//	return 0, fmt.Errorf("error in rows: %s", err.Error())
-//}
 
-//if err = rows.Scan(&id); err != nil {
-//	//err := tx.Rollback()
-//	//if err != nil {
-//	//	return 0, err
-//	//}
-//	log.Fatalln("error", err.Error())
-//	return id, err
-//}
-//fmt.Println(rows)
-//defer rows.Close()
-//queryComPost := fmt.Sprintf("INSERT INTO %s(`%v`)VALUES(%v)", tablePostCom, p_id, id)
-//_, err = p.db.Query(queryComPost)
-//if err != nil {
-//	return 0, err
-//}
-//_, err = tx.Exec(queryComPost)
-//if err != nil {
-//	log.Fatalln("failed", err.Error())
-//}
-//rows.Scan(&id)
-
-//	return 1, nil
-//}
